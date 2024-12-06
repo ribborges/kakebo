c95f283f1ffe50f421a035695c0f77061a51fdcd
Richard Borges <ribborges@outlook.com>
2024-12-05 23:09:52 -0300

    feat: update database initialization to include expense and income categories with default values

--- src/database/initializeDatabase.ts
+++ src/database/initializeDatabase.ts
@@ -8,21 +8,28 @@ export async function initializeDatabase(database: SQLiteDatabase) {
             color TEXT NOT NULL,
             icon TEXT NOT NULL
         );
+
+        INSERT INTO expense_categories (name, color, icon) VALUES ('Survival', '#ffe600', 'shopping-cart');
+        INSERT INTO expense_categories (name, color, icon) VALUES ('Leisure', '#00ccff', 'coffee');
+        INSERT INTO expense_categories (name, color, icon) VALUES ('Culture', '#ff00ff', 'book');
+        INSERT INTO expense_categories (name, color, icon) VALUES ('Extra', '#00ff00', 'dollar');
+
         CREATE TABLE IF NOT EXISTS income_categories (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             category TEXT NOT NULL
         );
+
+        INSERT INTO income_categories (category) VALUES ('Income');
+        INSERT INTO income_categories (category) VALUES ('Expense');
+        INSERT INTO income_categories (category) VALUES ('Saving');
+
         CREATE TABLE IF NOT EXISTS transactions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             value REAL NOT NULL,
-            type TEXT NOT NULL,
+            type INTEGER NOT NULL,
             category_id INTEGER NOT NULL,
             FOREIGN KEY (type) REFERENCES income_categories (id),
-            FOREIGN KEY (category_id) REFERENCES income_categories (id)
+            FOREIGN KEY (category_id) REFERENCES expense_categories (id)
         );
-        INSERT INTO categories (name, color, icon) VALUES ('Survival', '#ffe600', 'shopping-cart');
-        INSERT INTO categories (name, color, icon) VALUES ('Leisure', '#00ccff', 'coffee');
-        INSERT INTO categories (name, color, icon) VALUES ('Culture', '#ff00ff', 'book');
-        INSERT INTO categories (name, color, icon) VALUES ('Extra', '#00ff00', 'dollar');
     `)
 }
