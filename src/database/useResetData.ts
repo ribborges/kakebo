cd2d368d300cc48717f4fe4e13a0de50367cc570
Richard Borges <ribborges@outlook.com>
2024-12-16 22:52:27 -0300

    fix: simplify database reset by using execAsync for table drops

--- src/database/useResetData.ts
+++ src/database/useResetData.ts
@@ -5,19 +5,15 @@ export function useResetData() {
     const db = useSQLiteContext();
     
     async function deleteDatabase() {
-        const statement = await db.prepareAsync(`DROP TABLE IF EXISTS $table`);
-    
         try {
-            await statement.executeAsync({ $table: 'expense_categories' });
-            await statement.executeAsync({ $table: 'transaction_categories' });
-            await statement.executeAsync({ $table: 'transactions' });
+            await db.execAsync(`DROP TABLE IF EXISTS transaction_categories`);
+            await db.execAsync(`DROP TABLE IF EXISTS expense_categories`);
+            await db.execAsync(`DROP TABLE IF EXISTS transactions`);
             await db.execAsync(dbSchema);
 
             return (`Database has been reset`);
         } catch (error) {
             console.error(error);
-        } finally {
-            await statement.finalizeAsync();
         }
     }
 
