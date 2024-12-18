4b2f9d22de4f732f5b4f61e667f22d07f1b803b7
Richard Borges <ribborges@outlook.com>
2024-12-18 16:06:48 -0300

    feat: implement dynamic icon and color selection for NewCategory component

--- src/screens/NewCategory/index.tsx
+++ src/screens/NewCategory/index.tsx
@@ -6,6 +6,7 @@ import { FontAwesome } from "@expo/vector-icons";
 import { ACCENT_COLORS } from "@/constants/theme";
 import { Button } from "@/components/Button";
 import { useCategoriesDatabase } from "@/database/useCategoriesDatabase";
+import { categoryColors, categoryIcons } from "@/constants/categoryOptions";
 
 function NewCategory() {
     const [name, setName] = useState('');
@@ -55,102 +56,18 @@ function NewCategory() {
     return (
         <PanelContainer>
             <TextField label="Description" onChangeText={setName} value={name} />
-            <OptionSelector label="Icon" onChange={handleIconChange} onlyOne={true} options={[
-                {
-                    value: "address-book",
-                    children: <FontAwesome name="address-book" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "anchor",
-                    children: <FontAwesome name="anchor" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "bed",
-                    children: <FontAwesome name="bed" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "bell",
-                    children: <FontAwesome name="bell" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "bicycle",
-                    children: <FontAwesome name="bicycle" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "binoculars",
-                    children: <FontAwesome name="binoculars" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "birthday-cake",
-                    children: <FontAwesome name="birthday-cake" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "bus",
-                    children: <FontAwesome name="bus" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "camera",
-                    children: <FontAwesome name="camera" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "coffee",
-                    children: <FontAwesome name="coffee" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "compass",
-                    children: <FontAwesome name="compass" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "calculator",
-                    children: <FontAwesome name="calculator" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "car",
-                    children: <FontAwesome name="car" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "child",
-                    children: <FontAwesome name="child" size={16} color={ACCENT_COLORS.primary} />
-                }, {
-                    value: "cloud",
-                    children: <FontAwesome name="cloud" size={16} color={ACCENT_COLORS.primary} />
-                }
-            ]} />
-            <OptionSelector label="Color" onChange={handleColorChange} onlyOne={true} options={[
-                {
-                    value: "white",
-                    children: <FontAwesome name="circle" size={22} color={"#ffffff"} />
-                }, {
-                    value: "black",
-                    children: <FontAwesome name="circle" size={22} color={"#000000"} />
-                }, {
-                    value: "gray",
-                    children: <FontAwesome name="circle" size={22} color={"#808080"} />
-                }, {
-                    value: "red",
-                    children: <FontAwesome name="circle" size={22} color={"#ff0000"} />
-                }, {
-                    value: "green",
-                    children: <FontAwesome name="circle" size={22} color={"#00ff00"} />
-                }, {
-                    value: "blue",
-                    children: <FontAwesome name="circle" size={22} color={"#0000ff"} />
-                }, {
-                    value: "yellow",
-                    children: <FontAwesome name="circle" size={22} color={"#ffff00"} />
-                }, {
-                    value: "purple",
-                    children: <FontAwesome name="circle" size={22} color={"#ff00ff"} />
-                }, {
-                    value: "orange",
-                    children: <FontAwesome name="circle" size={22} color={"#ffa500"} />
-                }, {
-                    value: "pink",
-                    children: <FontAwesome name="circle" size={22} color={"#ffc0cb"} />
-                }, {
-                    value: "brown",
-                    children: <FontAwesome name="circle" size={22} color={"#a52a2a"} />
-                }, {
-                    value: "cyan",
-                    children: <FontAwesome name="circle" size={22} color={"#00ffff"} />
-                }, {
-                    value: "magenta",
-                    children: <FontAwesome name="circle" size={22} color={"#ff00ff"} />
-                }, {
-                    value: "lime",
-                    children: <FontAwesome name="circle" size={22} color={"#00ff00"} />
-                }, {
-                    value: "teal",
-                    children: <FontAwesome name="circle" size={22} color={"#008080"} />
-                }
-            ]} />
+            <OptionSelector label="Icon" onChange={handleIconChange} onlyOne={true} options={
+                categoryIcons.map((icon) => ({
+                    value: icon,
+                    children: <FontAwesome name={icon as any} size={16} color={ACCENT_COLORS.primary} />
+                }))
+            } />
+            <OptionSelector label="Color" onChange={handleColorChange} onlyOne={true} options={
+                categoryColors.map((color) => ({
+                    value: color,
+                    children: <FontAwesome name="circle" size={22} color={color} />
+                }))
+            } />
             <Button label="Save" onPress={() => handleSave()} />
         </PanelContainer>
     );
