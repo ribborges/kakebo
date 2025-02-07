e1e1ab93b11598da845392073aaae667d3a9a80d
Richard Borges <ribborges@outlook.com>
2025-02-06 23:27:38 -0300

    feat: add ProfilePic and UserInfo components for user profile display

---
+++ src/components/User/ProfilePic.tsx
@@ -0,0 +1,33 @@
+import { View, Image, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import clsx from "clsx";
+
+interface ProfilePicProps {
+    className?: string;
+    src?: string;
+    iconSize?: number;
+}
+
+export default function ProfilePic({ className, src, iconSize }: ProfilePicProps) {
+    return (
+        <View className={clsx(`
+            items-center justify-center
+            overflow-hidden
+            border-2 border-solid rounded-full
+            bg-zinc-100 dark:bg-zinc-900
+            border-zinc-200 dark:border-zinc-800`,
+            className
+        )}>
+            {
+                src
+                    ? <Image
+                        source={{ uri: src }}
+                        className={className}
+                    /> :
+                    <Text className="text-zinc-500 items-center justify-center">
+                        <MaterialCommunityIcons name="cat" size={iconSize} />
+                    </Text>
+            }
+        </View>
+    );
+}
