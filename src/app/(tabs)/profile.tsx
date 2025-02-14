2fd8e8fc760efbf34236451e7f30ea25310bce65
Richard Borges <ribborges@outlook.com>
2025-02-14 00:00:22 -0300

    chore: update icons and splash screen; update app version to 1.0.5

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -10,53 +10,66 @@ import { useCategoriesDatabase } from "@/database/useCategoriesDatabase";
 import { Spacer } from "@/components/Separator";
 
 export default function ProfilePage() {
-    const { setTransactions } = useTransactionStore();
-    const { setCategories } = useCategoryStore();
+  const { setTransactions } = useTransactionStore();
+  const { setCategories } = useCategoryStore();
 
-    const reset = useResetData();
-    const categoriesDb = useCategoriesDatabase();
+  const reset = useResetData();
+  const categoriesDb = useCategoriesDatabase();
 
-    const resetData = async () => {
-        try {
-            await reset.deleteDatabase();
-            setTransactions([]);
-            setCategories(await categoriesDb.list());
-            Alert.alert('Data has been reset');
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const resetData = async () => {
+    try {
+      await reset.deleteDatabase();
+      setTransactions([]);
+      setCategories(await categoriesDb.list());
+      Alert.alert("Data has been reset");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <View className="p-4 flex-1">
-            <UserTag />
-            <Spacer space={30} />
-            <PanelContainer className="g-2">
-                {/*
+  return (
+    <View className="p-4 flex-1">
+      <UserTag />
+      <Spacer space={30} />
+      <PanelContainer className="g-2">
+        {/*
                     <OptionButton
                         label="Settings"
                         icon={<FontAwesome name="gear" size={22} />}
                         onPress={() => Alert.alert('Settings')}
                     />
                 */}
-                {/*
+        {/*
                     <OptionButton
                         label="Financial history"
                         icon={<FontAwesome name="history" size={22} />}
                         onPress={() => Alert.alert('Financial history')}
                     />
                 */}
-                <OptionButton
-                    label="About"
-                    icon={<FontAwesome name="info" size={22} />}
-                    onPress={() => Alert.alert('About', 'This is a simple expense tracker app - v1.0.4', [{ text: 'GitHub', onPress: () => Linking.openURL('https://github.com/ribborges/kakebo') }, { text: 'OK' }])}
-                />
-                <OptionButton
-                    label="Reset data"
-                    icon={<FontAwesome name="trash" size={22} />}
-                    onPress={resetData}
-                />
-            </PanelContainer>
-        </View>
-    );
+        <OptionButton
+          label="About"
+          icon={<FontAwesome name="info" size={22} />}
+          onPress={() =>
+            Alert.alert(
+              "About",
+              "This is a simple expense tracker app - v1.0.5",
+              [
+                {
+                  text: "GitHub",
+                  onPress: () =>
+                    Linking.openURL("https://github.com/ribborges/kakebo"),
+                },
+                { text: "OK" },
+              ],
+            )
+          }
+        />
+        <OptionButton
+          label="Reset data"
+          icon={<FontAwesome name="trash" size={22} />}
+          onPress={resetData}
+        />
+      </PanelContainer>
+    </View>
+  );
 }
