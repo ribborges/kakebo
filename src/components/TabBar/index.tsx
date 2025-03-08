564f31bed341a917a1cb2443c592347ff6782ed8
Richard Borges <ribborges@outlook.com>
2025-03-08 13:18:50 -0300

    style: enhance TabBar component layout and blur effect

--- src/components/TabBar/index.tsx
+++ src/components/TabBar/index.tsx
@@ -30,64 +30,68 @@ function TabBarButton({ isFocused, label, icon, ...props }: tabBarBtnProps) {
 
 function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
     return (
-        <View className="
-            overflow-hidden
-            absolute bottom-2 self-center
-            bg-zinc-100/40 dark:bg-zinc-950/40
-            border border-t-2 border-solid rounded-full
-            border-zinc-200/40 dark:border-zinc-900/40
-        ">
-            <BlurView
-                intensity={15}
-                experimentalBlurMethod="dimezisBlurView"
-                className="flex-row justify-between items-center"
-            >
-                {state.routes.map((route, index) => {
-                    const { options } = descriptors[route.key];
-                    const label =
-                        options.tabBarLabel !== undefined
-                            ? options.tabBarLabel as string
-                            : options.title !== undefined
-                                ? options.title
-                                : route.name;
-                    const icon = options.tabBarIcon;
 
-                    if (['_sitemap', '+not-found'].includes(route.name)) return null;
+        <BlurView
+            intensity={20}
+            blurReductionFactor={20}
+            experimentalBlurMethod="dimezisBlurView"
+            className="
+                absolute bottom-2 overflow-hidden
+                self-center
+                border border-t-2 border-solid rounded-full
+                border-zinc-200/40 dark:border-zinc-900/40
+            "
+        >
+            <View className="flex-row justify-between items-center bg-zinc-100/40 dark:bg-zinc-950/40">
+                {
+                    state.routes.map((route, index) => {
+                        const { options } = descriptors[route.key];
+                        const label =
+                            options.tabBarLabel !== undefined
+                                ? options.tabBarLabel as string
+                                : options.title !== undefined
+                                    ? options.title
+                                    : route.name;
+                        const icon = options.tabBarIcon;
 
-                    const isFocused = state.index === index;
+                        if (['_sitemap', '+not-found'].includes(route.name)) return null;
 
-                    const onPress = () => {
-                        const event = navigation.emit({
-                            type: 'tabPress',
-                            target: route.key,
-                            canPreventDefault: true,
-                        });
+                        const isFocused = state.index === index;
 
-                        if (!isFocused && !event.defaultPrevented) {
-                            navigation.navigate(route.name, route.params);
-                        }
-                    };
+                        const onPress = () => {
+                            const event = navigation.emit({
+                                type: 'tabPress',
+                                target: route.key,
+                                canPreventDefault: true,
+                            });
 
-                    const onLongPress = () => {
-                        navigation.emit({
-                            type: 'tabLongPress',
-                            target: route.key,
-                        });
-                    };
+                            if (!isFocused && !event.defaultPrevented) {
+                                navigation.navigate(route.name, route.params);
+                            }
+                        };
 
-                    return (
-                        <TabBarButton
-                            key={route.name}
-                            icon={icon}
-                            onPress={onPress}
-                            onLongPress={onLongPress}
-                            isFocused={isFocused}
-                            label={label}
-                        />
-                    )
-                })}
-            </BlurView>
-        </View>
+                        const onLongPress = () => {
+                            navigation.emit({
+                                type: 'tabLongPress',
+                                target: route.key,
+                            });
+                        };
+
+                        return (
+                            <TabBarButton
+                                key={route.name}
+                                icon={icon}
+                                onPress={onPress}
+                                onLongPress={onLongPress}
+                                isFocused={isFocused}
+                                label={label}
+                            />
+                        )
+                    })
+                }
+            </View>
+        </BlurView>
+
     )
 }
 
